3369cf8b3da4576e80293c1ea602a93885740fe5
Pedro Algarvio <pedro@algarvio.me>
2013-01-25 20:18:08 +0000

    Fix tests, the run tests script and ubuntu git installs.
    * The run tests script now requires root permissions to execute.
    * Ubuntu git installs were starting upstarts services without checking if they were already running.
    * We now also check if upstart support exists and use upstarts files instead of the `init.d` files, ie, or upstart or `init.d`.
    * On Debian and Ubuntu we gather the packages to install(master, minion, syndic) and install them in one command.
    * We now detect if the script is running piped or not.

--- .run-boostrap-tests.sh
+++ .run-boostrap-tests.sh
@@ -17,29 +17,50 @@
 
 set -o nounset                              # Treat unset variables as an error
 
+
+if [ $(whoami) != "root" ] ; then
+    title="You need to run this script as root."
+    line="$(printf "%${COLUMNS}s" "")"
+    printf "\033[1;31m%*s\033[0m\n" $(((${#title}+$COLUMNS)/2)) "$title"
+    printf "\033[1;31m%s\033[0m\n" "${line// /*}"
+    exit 1
+fi
+
+
+# Change to the scripts parent directory
+cd $(dirname $0)
+
+# Find out the available columns on our tty
 COLUMNS=$(tput cols || 80)
 
+
 title_echo() {
-	title="$1"
-	line="$(printf "%${COLUMNS}s" "")"
-	printf "\033[0;33m%s\033[0m\n" "${line// /*}"
-	printf "\033[0;33m%*s\033[0m\n" $(((${#title}+$COLUMNS)/2)) "$title"
-#	printf "\033[1;34m%s\033[0m\n" "${line// /*}"
+    title="$1"
+    line="$(printf "%${COLUMNS}s" "")"
+    printf "\033[0;33m%s\033[0m\n" "${line// /*}"
+    printf "\033[0;33m%*s\033[0m\n" $(((${#title}+$COLUMNS)/2)) "$title"
 }
 
 failed_echo() {
-	title="FAILED"
-	line="$(printf "%${COLUMNS}s" "")"
-	printf "\033[1;31m%*s\033[0m\n" $(((${#title}+$COLUMNS)/2)) "$title"
-	printf "\033[1;31m%s\033[0m\n" "${line// /*}"
-	exit 1
+    title="FAILED"
+    line="$(printf "%${COLUMNS}s" "")"
+    printf "\033[1;31m%*s\033[0m\n" $(((${#title}+$COLUMNS)/2)) "$title"
+    printf "\033[1;31m%s\033[0m\n" "${line// /*}"
+    exit 1
 }
 
 passed_echo() {
-	title="OK"
-	line="$(printf "%${COLUMNS}s" "")"
-	printf "\033[1;32m%*s\033[0m\n" $(((${#title}+$COLUMNS)/2)) "$title"
-	printf "\033[1;32m%s\033[0m\n" "${line// /*}"
+    title="OK"
+    line="$(printf "%${COLUMNS}s" "")"
+    printf "\033[1;32m%*s\033[0m\n" $(((${#title}+$COLUMNS)/2)) "$title"
+    printf "\033[1;32m%s\033[0m\n" "${line// /*}"
+}
+
+cleanup() {
+    apt-get remove -y -o DPkg::Options::=--force-confold --purge salt-master salt-minion salt-syndic
+    apt-get autoremove -y -o DPkg::Options::=--force-confold --purge
+    [ -d /tmp/git ] && rm -rf /tmp/git
+    return 0
 }
 
 title_echo "Running checkbashisms"
@@ -48,31 +69,31 @@ title_echo "Running checkbashisms"
 title_echo "Passing '-N'(no minion) without passing '-M'(install master) or '-S'(install syndic) fails"
 ./bootstrap-salt-minion.sh -N && failed_echo || passed_echo
 
+title_echo "Using an unknown installation type fails"
+./bootstrap-salt-minion.sh foobar && failed_echo || passed_echo
+
 title_echo "Installing using bash"
-(sudo /bin/bash bootstrap-salt-minion.sh && salt-minion --versions-report && sudo apt-get remove salt-common salt-minion) && passed_echo || failed_echo
+(/bin/bash bootstrap-salt-minion.sh && salt-minion --versions-report && cleanup) && passed_echo || failed_echo
 
 title_echo "Installing using sh"
-(sudo ./bootstrap-salt-minion.sh && salt-minion --versions-report && sudo apt-get remove salt-common salt-minion) && passed_echo || failed_echo
+(./bootstrap-salt-minion.sh && salt-minion --versions-report && cleanup) && passed_echo || failed_echo
 
 title_echo "Installing stable with sh"
-(sudo ./bootstrap-salt-minion.sh stable && salt-minion --versions-report && sudo apt-get remove salt-common salt-minion) && passed_echo || failed_echo
+(./bootstrap-salt-minion.sh stable && salt-minion --versions-report && cleanup) && passed_echo || failed_echo
 
 title_echo "Installing ubuntu daily packages using sh"
-(sudo ./bootstrap-salt-minion.sh daily && salt-minion --versions-report && sudo apt-get remove salt-common salt-minion) && passed_echo || failed_echo
-
-title_echo "Using an unknown installation type fails"
-sudo ./bootstrap-salt-minion.sh foobar && failed_echo || passed_echo
+(./bootstrap-salt-minion.sh daily && salt-minion --versions-report && cleanup) && passed_echo || failed_echo
 
 title_echo "Installing stable piped through sh"
-(cat ./bootstrap-salt-minion.sh | sudo sh && salt-minion --versions-report && sudo apt-get remove salt-common salt-minion) && passed_echo || failed_echo
+(cat ./bootstrap-salt-minion.sh | sh && salt-minion --versions-report && cleanup) && passed_echo || failed_echo
 
 title_echo "Installing latest develop branch from git"
-(sudo ./bootstrap-salt-minion.sh git develop && salt --versions-report && sudo rm -rf /tmp/git ) && passed_echo || failed_echo
+(./bootstrap-salt-minion.sh git develop && salt --versions-report && cleanup ) && passed_echo || failed_echo
 
 title_echo "Installing from a specific git tag"
-(sudo ./bootstrap-salt-minion.sh git v0.12.1 && salt --versions-report && sudo rm -rf /tmp/git ) && passed_echo || failed_echo
+(./bootstrap-salt-minion.sh git v0.12.1 && salt --versions-report && cleanup ) && passed_echo || failed_echo
 
 title_echo "Installing from a specific git sha commit"
-(sudo ./bootstrap-salt-minion.sh git bf1d7dfb733a6133d6a750e0ab63a27e72cf7e81 && salt --versions-report && sudo rm -rf /tmp/git ) && passed_echo || failed_echo
+(./bootstrap-salt-minion.sh git bf1d7dfb733a6133d6a750e0ab63a27e72cf7e81 && salt --versions-report && cleanup ) && passed_echo || failed_echo
 
 exit 0

c2a7815cd361f6636cd78095c22c7aed0e3f918c
Pedro Algarvio <pedro@algarvio.me>
2013-03-17 23:42:06 +0000

    Should be `&&` not `||`.

--- bootstrap-salt.sh
+++ bootstrap-salt.sh
@@ -693,7 +693,7 @@ DISTRO_NAME_L=$(echo $DISTRO_NAME | tr '[:upper:]' '[:lower:]' | sed 's/[^a-zA-Z
 
 
 # Only Ubuntu has daily packages, let's let users know about that
-if ([ "${DISTRO_NAME_L}" != "ubuntu" ] && [ $ITYPE = "daily" ]) || \
+if ([ "${DISTRO_NAME_L}" != "ubuntu" ] && [ $ITYPE = "daily" ]) && \
    ([ "${DISTRO_NAME_L}" != "trisquel" ] && [ $ITYPE = "daily" ]); then
     echoerror "${DISTRO_NAME} does not have daily packages support"
     exit 1

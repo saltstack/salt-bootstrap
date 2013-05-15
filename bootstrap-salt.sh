dd69486c057190e6ad05d30afa0424b43348abd7
mguegan <mguegan@virtua.ch>
2013-05-15 06:27:02 +0200

    fix typo as referred by #128

--- bootstrap-salt.sh
+++ bootstrap-salt.sh
@@ -2540,7 +2540,7 @@ daemons_running() {
         [ $fname = "master" ] && [ $INSTALL_MASTER -eq $BS_FALSE ] && continue
         [ $fname = "syndic" ] && [ $INSTALL_SYNDIC -eq $BS_FALSE ] && continue
 
-        if [ ${DISTRO_NAME} == "SmartOS" ]; then
+        if [ "${DISTRO_NAME}" = "SmartOS" ]; then
             if [[ $(svcs -Ho STA salt-$fname) != "ON" ]]; then
                 echoerror "salt-$fname was not found running"
                 FAILED_DAEMONS=$(expr $FAILED_DAEMONS + 1)

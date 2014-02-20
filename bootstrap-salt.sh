355c0e44b0186e17f3fe7fb32caa25b29bfb92a0
Pedro Algarvio <pedro@algarvio.me>
2014-02-20 13:02:10 +0000

    CentOS now checks for enabled services

--- bootstrap-salt.sh
+++ bootstrap-salt.sh
@@ -2260,6 +2260,21 @@ install_centos_testing_post() {
     return 0
 }
 
+install_centos_check_services() {
+    if [ ! -f /sbin/initctl ]; then
+        return 0
+    fi
+
+    for fname in minion master syndic; do
+        # Skip if not meant to be installed
+        [ $fname = "minion" ] && [ $_INSTALL_MINION -eq $BS_FALSE ] && continue
+        [ $fname = "master" ] && [ $_INSTALL_MASTER -eq $BS_FALSE ] && continue
+        [ $fname = "syndic" ] && [ $_INSTALL_SYNDIC -eq $BS_FALSE ] && continue
+        __check_services_upstart salt-$fname || return 1
+    done
+    return 0
+}
+
 #
 #   Ended CentOS Install Functions
 #

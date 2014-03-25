82371b772c48f196c01279dffe28e916eeadc73b
Armagan Kimyonoglu <akimyonoglu@gmail.com>
2014-03-25 17:38:30 +0200

    don't use double square brackes

--- bootstrap-salt.sh
+++ bootstrap-salt.sh
@@ -2389,7 +2389,7 @@ install_red_hat_linux_stable_deps() {
     else
         OPTIONAL_ARCH=$CPU_ARCH_L
     fi
-    if [ $DISTRO_MAJOR_VERSION -eq 6 ] && [[ "X$(rhn-channel -l | grep optional)" != Xrhel-${OPTIONAL_ARCH}-server-optional-${DISTRO_MAJOR_VERSION}* ]]; then
+    if [ $DISTRO_MAJOR_VERSION -eq 6 ] && case "X$(rhn-channel -l | grep optional)" in Xrhel-${OPTIONAL_ARCH}-server-optional-${DISTRO_MAJOR_VERSION}* ) false ;; * ) true ;; esac ; then
       echoerror "Failed to find RHN optional repo, please enable it using the GUI or rhn-channel command."
       return 1
     fi

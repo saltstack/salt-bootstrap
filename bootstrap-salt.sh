saltstack/salt-bootstrap

commit 23b7809c83f53d6cac994318b632f71ed47d4335

Fix SC2045. Iterate over globs whenever possible.
Iterate over globs whenever possible (e.g. 'for f in */*.wav'), as for
loops over ls will fail for filenames like 'my file*.txt'.

bootstrap-salt.sh
@@ -3920,7 +3920,7 @@ preseed_master() {
     SEED_DEST="$_PKI_DIR/master/minions"
     [ -d $SEED_DEST ] || mkdir -p $SEED_DEST && chmod 700 $SEED_DEST || return 1
 
-    for keyfile in $(ls $_TEMP_KEYS_DIR); do
+    for keyfile in $_TEMP_KEYS_DIR/*; do
         src_keyfile="${_TEMP_KEYS_DIR}/${keyfile}"
         dst_keyfile="${SEED_DEST}/${keyfile}"
 

commit 18f42b648f3f4af4a8d12208a54c4df92b88c965

fix preseeding master
Fixes Issue #379 introduced in commit 23b7809c83f53d6cac994318b632f71ed47d4335

bootstrap-salt.sh
@@ -4006,6 +4006,7 @@ preseed_master() {
     [ -d $SEED_DEST ] || mkdir -p $SEED_DEST && chmod 700 $SEED_DEST || return 1
 
     for keyfile in $_TEMP_KEYS_DIR/*; do
+        keyfile=$(basename "${keyfile}")
         src_keyfile="${_TEMP_KEYS_DIR}/${keyfile}"
         dst_keyfile="${SEED_DEST}/${keyfile}"
 
7ef3a40bcf4a1ea068cbcf52c25ff16b9b496580
Mike Place <mp@saltstack.com>
2015-01-06 11:31:37 -0700

    Do a pacman db upgrade before installing deps

--- bootstrap-salt.sh
+++ bootstrap-salt.sh
@@ -3372,6 +3372,8 @@ install_amazon_linux_ami_testing_post() {
 #
 install_arch_linux_stable_deps() {
 
+    pacman-db-upgrade
+
     if [ ! -f /etc/pacman.d/gnupg ]; then
         pacman-key --init && pacman-key --populate archlinux || return 1
     fi

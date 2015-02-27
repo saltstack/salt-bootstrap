aa7c077fa97c3b0528a163972934581c693c2ff1
Pedro Algarvio <pedro@algarvio.me>
2015-02-27 10:45:41 +0000

    Fixed backports URL

--- bootstrap-salt.sh
+++ bootstrap-salt.sh
@@ -2099,8 +2099,8 @@ install_debian_7_deps() {
     __apt_get_install_noinput debian-archive-keyring && apt-get update
 
     # Debian Backports
-    if [ "$(grep -R 'backports.debian.org' /etc/apt | grep -v "^#")" = "" ]; then
-        echo "deb http://backports.debian.org/debian-backports wheezy-backports main" >> \
+    if [ "$(grep -R 'wheezy-backports' /etc/apt | grep -v "^#")" = "" ]; then
+        echo "deb http://http.debian.net/debian wheezy-backports main" >> \
             /etc/apt/sources.list.d/backports.list
 
         # Add the backports key

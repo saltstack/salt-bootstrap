69158f696fa0662c7d12ea2f25841424e6a3d0f0
Sebastian Wendel <s.wendel@sourceindex.de>
2017-07-25 16:41:11 +0200

    fallback to initial ScriptVersion nummber as requested from @rallytime

--- bootstrap-salt.sh
+++ bootstrap-salt.sh
@@ -18,7 +18,7 @@
 #======================================================================================================================
 set -o nounset                              # Treat unset variables as an error
 
-__ScriptVersion="2017.07.25"
+__ScriptVersion="2017.05.24"
 __ScriptName="bootstrap-salt.sh"
 
 __ScriptFullName="$0"

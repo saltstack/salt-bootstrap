1dacb5eb33f72d4006762b11edc8ce711297a5dc
Ch3LL <megan.wilhite@gmail.com>
2018-08-15 14:19:21 -0400

    Changelog for 2018.08.15 release

--- bootstrap-salt.sh
+++ bootstrap-salt.sh
@@ -18,7 +18,7 @@
 #======================================================================================================================
 set -o nounset                              # Treat unset variables as an error
 
-__ScriptVersion="2018.08.13"
+__ScriptVersion="2018.08.15"
 __ScriptName="bootstrap-salt.sh"
 
 __ScriptFullName="$0"

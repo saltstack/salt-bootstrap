274af65d5fe5f08947edfba6c6429e9dd0d10dc3
kgbsd <45101782+kgbsd@users.noreply.github.com>
2019-11-18 07:28:31 -0700

    Update bootstrap-salt.sh

--- bootstrap-salt.sh
+++ bootstrap-salt.sh
@@ -5293,7 +5293,7 @@ install_freebsd_git_deps() {
         /usr/local/sbin/pkg install -y git || return 1
     fi
 
-    /usr/local/sbin/pkg install -y www/py-requests || return 1
+    /usr/local/sbin/pkg install -y www/py36-requests || return 1
 
     __git_clone_and_checkout || return 1
 

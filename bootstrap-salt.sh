7c0e4fca7bd1ccc7d493179cde9f3d408388d5aa
Pedro Algarvio <pedro@algarvio.me>
2020-01-29 16:20:08 +0000

    FreeBSD versions lower than 11 are EOL and unsupported

--- bootstrap-salt.sh
+++ bootstrap-salt.sh
@@ -1688,10 +1688,9 @@ __check_end_of_life_versions() {
             ;;
 
         freebsd)
-            # FreeBSD versions lower than 9.1 are not supported.
-            if { [ "$DISTRO_MAJOR_VERSION" -eq 9 ] && [ "$DISTRO_MINOR_VERSION" -lt 01 ]; } || \
-                [ "$DISTRO_MAJOR_VERSION" -lt 9 ]; then
-                echoerror "Versions lower than FreeBSD 9.1 are not supported."
+            # FreeBSD versions lower than 11 are EOL
+            if [ "$DISTRO_MAJOR_VERSION" -lt 11 ]; then
+                echoerror "Versions lower than FreeBSD 11 are EOL and no longer supported."
                 exit 1
             fi
             ;;

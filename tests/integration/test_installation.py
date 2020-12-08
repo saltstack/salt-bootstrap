c291d741f55119377c046b0d6b9c5b863c72d4eb
Bryce Larson <brycel@vmware.com>
2020-12-08 22:22:46 +0000

    only run gentoo on branch, black everything

--- tests/integration/test_installation.py
+++ tests/integration/test_installation.py
@@ -9,16 +9,16 @@ log = logging.getLogger(__name__)
 
 def test_ping(host):
     with host.sudo():
-        assert host.salt('test.ping', '--timeout=120')
+        assert host.salt("test.ping", "--timeout=120")
 
 
 def test_target_python_version(host, target_python_version):
     with host.sudo():
-        ret = host.salt('grains.item', 'pythonversion', '--timeout=120')
+        ret = host.salt("grains.item", "pythonversion", "--timeout=120")
         assert ret["pythonversion"][0] == target_python_version
 
 
 def test_target_salt_version(host, target_salt_version):
     with host.sudo():
-        ret = host.salt('grains.item', 'saltversion', '--timeout=120')
+        ret = host.salt("grains.item", "saltversion", "--timeout=120")
         assert ret["saltversion"].startswith(target_salt_version)

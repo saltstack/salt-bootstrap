13ed3c527ba21ab2da0dac1f1560da05383eeaaa
Pedro Algarvio <pedro@algarvio.me>
2021-03-02 07:25:01 +0000

    Update version for release

--- bootstrap-salt.sh
+++ bootstrap-salt.sh
@@ -23,7 +23,7 @@
 #======================================================================================================================
 set -o nounset                              # Treat unset variables as an error
 
-__ScriptVersion="2020.10.20"
+__ScriptVersion="2021.03.02"
 __ScriptName="bootstrap-salt.sh"
 
 __ScriptFullName="$0"

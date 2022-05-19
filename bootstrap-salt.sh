8636d9413092654c91303d64a660841b2e9b2116
krionbsd <krion@FreeBSD.org>
2022-05-19 14:17:19 +0200

    Bump version to 2022.05.19 release

--- bootstrap-salt.sh
+++ bootstrap-salt.sh
@@ -23,7 +23,7 @@
 #======================================================================================================================
 set -o nounset                              # Treat unset variables as an error
 
-__ScriptVersion="2022.03.15"
+__ScriptVersion="2022.05.19"
 __ScriptName="bootstrap-salt.sh"
 
 __ScriptFullName="$0"

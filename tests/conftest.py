9818c45d8826e898a95853e7e69cd4a280198212
Imran Iqbal <iqbalmy@hotmail.com>
2022-06-08 09:10:12 +0100

    test(conftest): adjust `PATH` on macOS jobs only (not Vagrant)

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ log = logging.getLogger(__name__)
 
 @pytest.fixture(scope="session")
 def host():
-    if os.environ.get("RUNNER_OS", "") == "macOS":
+    if os.environ.get("RUNNER_OS", "") == "macOS" and os.environ.get("KITCHEN_LOCAL_YAML", "") == "kitchen.macos.yml":
         # Adjust the `PATH` so that the `salt-call` executable can be found
         os.environ["PATH"] = "/opt/salt/bin{}{}".format(os.pathsep, os.environ["PATH"])
         return testinfra.get_host("local://", sudo=True)

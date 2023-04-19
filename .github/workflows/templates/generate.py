d8a7e90953368affa335b6837c34d67ecbc27a2c
Gareth J. Greenaway <gareth@saltstack.com>
2023-04-18 19:51:33 -0700

    fixing 3006 tag name

--- .github/workflows/templates/generate.py
+++ .github/workflows/templates/generate.py
@@ -263,7 +263,7 @@ VERSION_DISPLAY_NAMES = {
     "3003": "v3003",
     "3004": "v3004",
     "3005": "v3005",
-    "3006": "v3006",
+    "3006": "v3006.0",
     "master": "Master",
     "latest": "Latest",
     "nightly": "Nightly",

33dc0c4753e6072c87cd4cdee15712b99f8d2500
twangboy <shane.d.lee@gmail.com>
2023-09-22 13:02:52 -0600

    Fix global variable

--- salt-quick-start.ps1
+++ salt-quick-start.ps1
@@ -84,7 +84,7 @@ function Expand-ZipFile {
 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls12'
 
-$global:$ProgressPreference = 'SilentlyContinue'
+$global:ProgressPreference = 'SilentlyContinue'
 
 $RepoUrl = "https://repo.saltproject.io/salt/py3/onedir"
 

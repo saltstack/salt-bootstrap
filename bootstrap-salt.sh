2b3080c737f40ba66ded7cd9f93607300615bfa5
Salt Project Packaging <saltproject-packaging@vmware.com>
2023-11-07 18:35:16 +0000

    Update develop branch for the v2023.11.07 release

--- bootstrap-salt.sh
+++ bootstrap-salt.sh
@@ -23,7 +23,7 @@
 #======================================================================================================================
 set -o nounset                              # Treat unset variables as an error
 
-__ScriptVersion="2023.08.03"
+__ScriptVersion="2023.11.07"
 __ScriptName="bootstrap-salt.sh"
 
 __ScriptFullName="$0"

cecea60a06312057d55d6348d80552b3108a2ea5
Dennis Dudek <100142218+Dudek-AMS@users.noreply.github.com>
2024-01-09 19:47:53 +0100

    Update bootstrap-salt.sh
    fixed missing quotes

--- bootstrap-salt.sh
+++ bootstrap-salt.sh
@@ -3663,7 +3663,7 @@ __install_saltstack_debian_repository() {
     SALTSTACK_DEBIAN_URL="${HTTP_VAL}://${_REPO_URL}/${__PY_VERSION_REPO}/debian/${DEBIAN_RELEASE}/${__REPO_ARCH}"
     echo "$__REPO_ARCH_DEB $SALTSTACK_DEBIAN_URL $DEBIAN_CODENAME main" > "/etc/apt/sources.list.d/salt.list"
 
-    __apt_key_fetch "$SALTSTACK_DEBIAN_URL/SALT-PROJECT-GPG-PUBKEY-2023.gpg || return 1
+    __apt_key_fetch "$SALTSTACK_DEBIAN_URL/SALT-PROJECT-GPG-PUBKEY-2023.gpg" || return 1
 
     __wait_for_apt apt-get update || return 1
 }

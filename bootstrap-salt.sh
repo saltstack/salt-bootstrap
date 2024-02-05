6ddcf5dd9491d2c9dabd3d21a36c9cb2f556fbe7
David Murphy <dmurphy@saltstack.com>
2024-02-05 15:18:53 -0700

    Test build to check PR process

--- bootstrap-salt.sh
+++ bootstrap-salt.sh
@@ -23,7 +23,7 @@
 #======================================================================================================================
 set -o nounset                              # Treat unset variables as an error
 
-__ScriptVersion="2024.01.04"
+__ScriptVersion="2024.02.05"
 __ScriptName="bootstrap-salt.sh"
 
 __ScriptFullName="$0"

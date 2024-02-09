2f93d4c1ea7c99512de5c0fb583f5c1ff62cf1da
David Murphy <dmurphy@saltstack.com>
2024-02-08 18:05:30 -0700

    Removed use of yum-utils and dnf-utils (doesn't exist)

--- bootstrap-salt.sh
+++ bootstrap-salt.sh
@@ -4461,8 +4461,7 @@ install_fedora_onedir_deps() {
     ## DGM     __PACKAGES="yum-utils chkconfig"
     ## DGM fi
 
-
-    ## DGM __PACKAGES="yum-utils chkconfig procps"
+    ## DGM __PACKAGES="${__PACKAGES} chkconfig procps"
 
     __PACKAGES="chkconfig procps"
 
@@ -4601,7 +4600,17 @@ install_centos_stable_deps() {
         __install_saltstack_rhel_onedir_repository || return 1
     fi
 
-    __PACKAGES="yum-utils chkconfig procps"
+    ## DGM can find no dnf-utils in Fedora packaging archives and yum-utils EL7 and F30, none after
+    ## DGM but find it on 8 and 9 Centos Stream, and Alma 8 & 9 but versions we are using doesn't have them
+    ## DGM and probably don't need these packages since using onedir
+    ## DGM if [ "$DISTRO_MAJOR_VERSION" -ge 8 ]; then
+    ## DGM     __PACKAGES="dnf-utils chkconfig"
+    ## DGM else
+    ## DGM     __PACKAGES="yum-utils chkconfig"
+    ## DGM fi
+    ## DGM __PACKAGES="${__PACKAGES} chkconfig procps"
+
+    __PACKAGES="chkconfig procps"
 
     # shellcheck disable=SC2086
     __yum_install_noinput "${__PACKAGES}" || return 1

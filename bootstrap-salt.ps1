68f6f8b8548ab06068b89b48f7345afcc2ba12af
Salt Project Packaging <saltproject-packaging@vmware.com>
2026-01-15 19:03:59 +0000

    Update develop branch for the v2026.01.15 release

--- bootstrap-salt.ps1
+++ bootstrap-salt.ps1
@@ -108,7 +108,7 @@ if ($help) {
     exit 0
 }
 
-$__ScriptVersion = "2025.12.05"
+$__ScriptVersion = "2026.01.15"
 $ScriptName = $myInvocation.MyCommand.Name
 
 # We'll check for the Version next, because it also has no requirements
